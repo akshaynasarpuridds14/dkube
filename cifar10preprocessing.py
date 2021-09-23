3ab522828ef0a5be45f7a96692f0e890f19462f5
Akshay Nasarpuri <akshaynasarpuri@gmail.com>
2021-09-23 09:45:30 +0000

    adding preprocessing file

---
+++ cifar10preprocessing.py
@@ -0,0 +1,19 @@
+DATA_DIR='/opt/dkube/input'
+
+path=DATA_DIR+'/train.7z'
+path_labels=DATA_DIR+'/trainLabels.csv'
+
+import py7zr
+with py7zr.SevenZipFile(path, mode='r') as z:
+    z.extractall(path="/opt/dkube/output")
+    
+# importing shutil module
+import shutil
+ 
+# Source path
+source = path_labels
+ 
+# Destination path
+destination = "/opt/dkube/output"
+
+shutil.copyfile(source, destination)
