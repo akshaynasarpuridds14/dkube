580b072f278b90bbde727a5994b6a66582046815
Akshay Nasarpuri <akshaynasarpuri@gmail.com>
2021-09-23 10:05:45 +0000

    updating preprocessing file

--- cifar10preprocessing.py
+++ cifar10preprocessing.py
@@ -14,6 +14,6 @@ import shutil
 source = path_labels
  
 # Destination path
-destination = "/opt/dkube/output"
+destination = "/opt/dkube/output/trainLabels.csv"
 
 shutil.copyfile(source, destination)
